d76c9f8fc7be26c6ba42651b289994ae634796d5
Quang Ninh <qndt123@gmail.com>
2023-04-06 22:15:26 +0700

    Make OpenGL always select NVIDIA GPU or AMD GPU

--- src/common.hpp
+++ src/common.hpp
@@ -41,7 +41,7 @@
 #ifdef CYBRION_DEBUG
 #define CYBRION_ROOT_PATH "D:/github/cybrion/"
 #else
-#define CYBRION_ROOT_PATH "D:/github/cybrion/"
+#define CYBRION_ROOT_PATH
 #endif
 
 namespace cybrion
---
+++ src/gpu.cpp
@@ -0,0 +1,6 @@
+#include <Windows.h>
+
+extern "C" {
+    _declspec(dllexport) DWORD NvOptimusEnablement = 1;
+    _declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
